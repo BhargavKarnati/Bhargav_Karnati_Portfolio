09142de7aa517f1bfee3977ce703695f5e6727b6
Bhargav Karnati <karnatibhargav19@gmail.com>
2025-07-09 15:31:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Bhargav's Portfolio</h1>
+      <p>This is the starter page. More customization coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
